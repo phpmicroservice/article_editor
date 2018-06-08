cd5234591dd4ba4887f36495ada05a0959d386b7
hpy <540994598@qq.com>
2018-06-08 09:07:40 +0800

    aa

--- src/config/xitong.js
+++ src/config/xitong.js
@@ -1,4 +1,4 @@
 export default {
-      scoket_url: "ws://192.168.1.220:34504/",
-      file_url : "http://ykm_file_web.s237.psd1412.com",
-  };
+        scoket_url: "ws://192.168.1.221:34504/",
+        file_url: "http://ykm.toplink.com"  
+};
