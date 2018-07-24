b8d394bfbf0102352d4731f6011eb9d9d5b6df55
hpy <540994598@qq.com>
2018-07-24 10:41:53 +0800

    删除测试图片

--- src/components/imglist.vue
+++ src/components/imglist.vue
@@ -50,16 +50,6 @@
 
 <script>
 import config from "@/config/config"
-import img1 from "@/assets/images/1.jpg"
-import img2 from "@/assets/images/2.jpg"
-import img3 from "@/assets/images/3.jpg"
-import img4 from "@/assets/images/4.jpg"
-import img5 from "@/assets/images/5.jpg"
-import img6 from "@/assets/images/6.jpg"
-import img7 from "@/assets/images/7.jpg"
-import img8 from "@/assets/images/8.jpg"
-import img9 from "@/assets/images/9.jpg"
-import img10 from "@/assets/images/10.jpg"
 export default {
     data(){
         return {
